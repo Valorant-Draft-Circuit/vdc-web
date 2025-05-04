65f71229dbf5b8c41119893aa2dc939c31403f9f
Sooho <sooho.lim4@gmail.com>
2025-05-03 23:13:21 -0700

    hide icons on desktop

--- app/components/auth/SignIn.tsx
+++ app/components/auth/SignIn.tsx
@@ -10,7 +10,7 @@ export default function SignIn() {
         type="submit"
         className="flex flex-row space-x-2 xl:bg-vdcRed rounded-sm px-4 py-1 gap-4 text-sm font-semibold text-gray-300 xl:text-vdcWhite shadow-xs hover:text-vdcRed xl:hover:bg-red-500 hover:cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
       >
-        <ArrowRightEndOnRectangleIcon className="m-auto w-5" />
+        <ArrowRightEndOnRectangleIcon className="m-auto w-5 xl:hidden" />
         <h1 className="italic text-lg xl:text-md 4xl:text-2xl">Sign In</h1>
       </button>
     </form>
--- app/components/auth/SignOut.tsx
+++ app/components/auth/SignOut.tsx
@@ -10,7 +10,7 @@ export default function SignOut() {
         type="submit"
         className="xl:bg-vdcRed rounded-sm px-4 py-1 text-sm flex flex-row gap-1 font-semibold text-gray-300 xl:text-vdcWhite shadow-xs xl:hover:bg-red-500 hover:text-vdcRed hover:cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
       >
-        <ArrowRightStartOnRectangleIcon className="m-auto w-5" />
+        <ArrowRightStartOnRectangleIcon className="m-auto w-5 xl:hidden" />
         <h1 className="italic text-lg xl:text-md 4xl:text-2xl">Sign Out</h1>
       </button>
     </form>
