96f47f333db65674221e24f91a184a5f9ed96d7c
Sooho <sooho.lim4@gmail.com>
2025-05-07 19:24:00 -0700

    limit discord's scope for auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,5 +2,10 @@ import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord],
+  providers: [
+    Discord({
+      authorization:
+        "https://discord.com/api/oauth2/authorize?scope=identify+guilds.join",
+    }),
+  ],
 });
