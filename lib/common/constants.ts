c845a009147144dd6fbe6c86874f15b226fe94ad
Sooho <sooho.lim4@gmail.com>
2025-05-08 23:36:00 -0700

    franchise tier indicator

--- lib/common/constants.ts
+++ lib/common/constants.ts
@@ -1,3 +1,5 @@
+import { Tier } from "@prisma/client";
+
 const BUCKET_URL = "https://uni-objects.nyc3.cdn.digitaloceanspaces.com/vdc/";
 export const DISCORD_LINK = "https://go.vdc.gg/discord";
 export const RULEBOOK_URL = "https://blog.vdc.gg/rulebook/";
@@ -5,3 +7,15 @@ export const BEHAVIOR_GUIDELINE_URL =
   "https://docs.google.com/spreadsheets/d/14wmSU43cB2xf9IOCuW0-74Ec8AXt6I6UGZHJhDNJJGc/edit?gid=0#gid=0";
 export const WEB_ASSET_URL = BUCKET_URL + "web-assets/";
 export const TEAM_LOGOS_URL = BUCKET_URL + "team-logos/";
+export const VDC_PURPLE = "#9b59b6";
+export const VDC_BLUE = "#3498db";
+export const VDC_GREEN = "#2ecc71";
+export const VDC_YELLOW = "#f1c40f";
+
+export const TIER_COLOR_MAP: Record<Tier, string> = {
+  MYTHIC: VDC_PURPLE,
+  EXPERT: VDC_BLUE,
+  APPRENTICE: VDC_GREEN,
+  PROSPECT: VDC_YELLOW,
+  MIXED: ""
+};
