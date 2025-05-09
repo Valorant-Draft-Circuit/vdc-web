addbef7a802cac7627ab07bc3a480363f0951484
Sooho <sooho.lim4@gmail.com>
2025-05-08 20:21:32 -0700

    add page description

--- app/about/franchises/page.tsx
+++ app/about/franchises/page.tsx
@@ -20,9 +20,14 @@ export default async function Page() {
             height={5000}
             className="absolute inset-0 -z-10 size-full object-cover sm:object-top lg:object-[10%_10%] brightness-20"
           />
-          <h1 className="text-3xl italic font-semibold tracking-tight text-pretty text-center text-vdcRed xl:text-4xl">
+          <h1 className="text-3xl italic font-semibold tracking-tight text-pretty text-vdcRed xl:text-4xl">
             Season {currentSeason} franchises
           </h1>
+          <h2 className="mt-4 text-base/7 text-pretty text-vdcWhite italic">
+            These following franchises and their teams battle for the VDC S{currentSeason} trophy
+            in each tier. <br />
+            Click on a franchise to learn more about them!
+          </h2>
         </div>
 
         <div className="mt-10 xl:mt-5 flex flex-col mx-2 p-5 rounded-xl gap-2 drop-shadow-2xl xl:grid xl:grid-cols-3 xl:gap-4">
--- components/franchises/FranchiseCard.tsx
+++ components/franchises/FranchiseCard.tsx
@@ -7,12 +7,13 @@ export default function FranchiseCard({ franchise }: { franchise }) {
   const secondary = toHexcode(franchise.Brand.colorSecondary);
 
   return (
-    <div
-      style={{ "--p": primary, "--s": secondary } as React.CSSProperties}
-      className="flex flex-row py-4 px-5 rounded-2xl h-28 xl:m-auto xl:h-32 xl:w-68 xl:justify-center items-center drop-shadow-xl bg-gradient-to-tl from-[var(--p)] to-[var(--s)] hover:via-[#ffffff] hover:from-[var(--s)] hover:to-[var(--p)] transition-colors duration-900 ease-in-out"
-    >
-      <div className="absolute inset-0 bg-black/40 rounded-2xl pointer-events-none transition-opacity duration-800" />
-      <Link href={`/about/franchises/${franchise.slug}`}>
+    <Link href={`/about/franchises/${franchise.slug}`}>
+      <div
+        style={{ "--p": primary, "--s": secondary } as React.CSSProperties}
+        className="flex flex-row py-4 px-5 rounded-2xl h-28 xl:m-auto xl:h-32 xl:w-68 xl:justify-center items-center drop-shadow-xl bg-gradient-to-tl from-[var(--p)] to-[var(--s)] hover:from-[var(--s)] hover:to-[var(--p)] transition-colors duration-800 ease-in-out"
+      >
+        <div className="absolute inset-0 bg-black/40 rounded-2xl pointer-events-none transition-opacity duration-800" />
+
         <div className="flex flex-row gap-4 text-center xl:gap-6">
           <div className="drop-shadow-lg flex">
             <Image
@@ -23,13 +24,13 @@ export default function FranchiseCard({ franchise }: { franchise }) {
               className="w-24 h-auto xl:w-30"
             />
           </div>
-          <div className="flex flex-col text-start my-auto xl:text-center italic w-full text-md sm:m-auto drop-shadow-2xl text-vdcWhite">
+          <div className="flex flex-col text-start my-auto italic w-full text-md sm:m-auto drop-shadow-2xl text-vdcWhite">
             <h1 className="xl:hidden">{franchise.slug}</h1>
             <h1 className="text-lg">{franchise.name}</h1>
           </div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
 
