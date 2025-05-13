6f80b2114ee2bdc939df089a1badb5e5fc729052
Sooho <sooho.lim4@gmail.com>
2025-05-12 21:19:24 -0700

    multiple ways to search

---
+++ app/profile/[player]/page.tsx
@@ -0,0 +1,39 @@
+import Player from "@/components/profile/Player";
+import PlayerNotFound from "@/components/profile/PlayerNotFound";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ player: string }>;
+}) {
+  const ENCODED_DIVIDER = encodeURIComponent("#");
+  const NUMBER_REGEX = /^\d+$/;
+  const { player } = await params;
+  let playerRiotIGN: string;
+  if (NUMBER_REGEX.test(player)) return await handleDiscordIDSearch(player);
+  else if (player.includes(ENCODED_DIVIDER))
+    playerRiotIGN = decodeURIComponent(player);
+  else if (player.includes("-")) {
+    const playerSplit = player.split("-");
+    playerRiotIGN = decodeURIComponent(`${playerSplit[0]}#${playerSplit[1]}`);
+  } else {
+    return <PlayerNotFound player={decodeURIComponent(player)} />;
+  }
+  return <h1>{playerRiotIGN}</h1>;
+}
+
+async function handleDiscordIDSearch(discordID: string) {
+  // TODO: rename api so its clear we are searching by discordID
+  const res = await fetch(`${process.env.URL}/api/users/${discordID}`);
+  if (res.ok) {
+    const riotIGN: string = await res.json();
+    const encodedIGN = encodeURIComponent(riotIGN);
+    redirect(`/profile/${encodedIGN}`);
+  } else {
+    return <PlayerNotFound player={discordID} />;
+  }
+}
+
+async function handleRiotIgnSearch(player) {}
