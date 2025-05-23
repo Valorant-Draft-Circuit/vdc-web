b953653fc27ffc7e49cef146407d342a4c602492
Sooho <sooho.lim4@gmail.com>
2025-05-22 17:21:03 -0700

    matchScores sometimes doesnt show up for some reason

--- components/schedule/MatchCard.tsx
+++ components/schedule/MatchCard.tsx
@@ -26,13 +26,14 @@ type Match = {
 export default function MatchCard({ match }: { match: Match }) {
   const goToMatch = () => router.push(`/match/${match.id}`);
   const router = useRouter();
+
   return (
     <div
       onClick={goToMatch}
       className="flex flex-row h-22 gap-8 sm:gap-3 py-3 w-full xl:px-24 m-auto justify-evenly rounded-2xl bg-vdcWhite dark:bg-vdcGrey drop-shadow-lg hover:cursor-pointer hover:opacity-98 transition-opacity ease-in-out duration-150"
     >
       <HomeBadge home={match.Home} tier={match.tier} />
-      <MatchScore homeWins={match.homeWins!} awayWins={match.awayWins!} />
+      <MatchScore homeWins={match.homeWins} awayWins={match.awayWins} />
       <AwayBadge away={match.Away} tier={match.tier} />
     </div>
   );
