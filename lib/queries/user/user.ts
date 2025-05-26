131f667d6d646a4b255fd778a3459ab1cba9185b
Sooho <sooho.lim4@gmail.com>
2025-05-25 21:11:58 -0700

    user dlatngh accountID until auth and db are connected

--- lib/queries/user/user.ts
+++ lib/queries/user/user.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/prisma/prismadb";
 export async function getUser(id) {
   const user = await prisma.user.findUnique({
     where: {
-      id: "clu6eh21r0000klf6uyfmowyj",
+      id: "clu76oynz0000traw15nj9btz",
     },
     include: {
       Accounts: {
