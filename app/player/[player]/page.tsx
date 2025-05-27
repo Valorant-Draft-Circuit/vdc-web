67e054b7a16e3133ae836d41b8b97988e3163e42
Sooho <95513590+dlatngh@users.noreply.github.com>
2025-05-26 22:42:31 -0700

    bunch of feature additions that comes with auth adapter (#51)

--- app/player/[player]/page.tsx
+++ app/player/[player]/page.tsx
@@ -134,7 +134,7 @@ async function getPlayerByRiot(riotIGN) {
 
 function listAllSeasons(currentSeason: number) {
   const seasons: string[] = [];
-  for (let i = currentSeason; i >= 1; i--) {
+  for (let i = currentSeason; i >= 6; i--) {
     seasons.push(String(i));
   }
   return seasons;
