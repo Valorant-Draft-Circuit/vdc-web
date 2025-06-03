f2b20fbb86d2ebd22ef7fbef6db410e536686b57
Sooho <sooho.lim4@gmail.com>
2025-06-02 19:11:03 -0700

    add/remove preview specific features

--- components/home/HowToPlay.tsx
+++ components/home/HowToPlay.tsx
@@ -24,6 +24,7 @@ const steps = [
           href={DISCORD_LINK}
           className="text-vdcRed underline"
           target="_blank"
+          rel="noopener noreferrer"
         >
           Discord!
         </a>
@@ -116,7 +117,7 @@ export default function HowToPlay() {
                 )}
                 {button && (
                   <div className="mt-2">
-                    <DiscordButton text="Discord" />
+                    <DiscordButton text="Discord" signInButton={false} />
                   </div>
                 )}
               </div>
--- components/navigation/side/SideBar.tsx
+++ components/navigation/side/SideBar.tsx
@@ -1,10 +1,12 @@
 import SideLinks from "./SideLinks";
 
 export default function SideBar() {
+  const preview = Boolean(process.env.PREVIEW);
+
   return (
     <nav className="xl:hidden shadow-2xl">
       <div className="flex top-0 fixed z-50" id="slider">
-        <SideLinks />
+        <SideLinks preview={preview} />
       </div>
     </nav>
   );
