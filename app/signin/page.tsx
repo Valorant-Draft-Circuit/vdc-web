057540db9abb2ae7150bcb3be4a5f6bcfef58c0c
forg <jessi.nappi220@gmail.com>
2025-06-06 21:04:00 -0400

    removed "powered by next auth" and implemented the use of existing DiscordButton

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,16 +1,12 @@
 import Image from "next/image"
 import { signIn } from '@/lib/auth/auth'
+import DiscordButton from "@/components/buttons/DiscordButton";
 
 
 export default function SignInPage() {
-    async function handleSignIn() {
-        'use server';
-        await signIn('discord', { redirectTo: '/'})
-    }
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-vdcWhite dark:bg-vdcBlack">
-      <div className="bg-vdcLtGrey dark:bg-vdcGray shadow-lg rounded-xl p-10 text-center max-w-sm w-full">
+      <div className="bg-vdcWhite dark:bg-vdcBlack shadow-lg rounded-xl p-10 text-center max-w-sm w-full">
         <div className="flex justify-center mb-6">
           <Image
                 alt="vdc fl"
@@ -20,26 +16,10 @@ export default function SignInPage() {
             />
         </div>
         <h1 className="text-2xl font-bold mb-2">Sign in With Discord</h1>
-        <form action={handleSignIn}>
-            <button
-                type="submit"
-                className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded-lg w-full"
-            >
-                Sign In with Discord
-            </button>
-        </form>
+        <div>
+          <DiscordButton text="Sign In with Discord" signInButton={true} />
+        </div>
       </div>
-
-      <footer className="mt-8 text-sm text-gray-500">
-        Powered by{' '}
-        <a
-          href="https://next-auth.js.org/"
-          className="underline"
-          target="_blank"
-        >
-          NextAuth.js
-        </a>
-      </footer>
     </div>
   );
 }
