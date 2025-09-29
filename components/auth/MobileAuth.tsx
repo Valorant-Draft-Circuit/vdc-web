a6f7437f21272aa8b598014a5a4a74d1276af5d7
Sooho <95513590+dlatngh@users.noreply.github.com>
2025-09-28 20:02:51 -0700

    add user data to mobile auth section (#129)

--- components/auth/MobileAuth.tsx
+++ components/auth/MobileAuth.tsx
@@ -5,9 +5,35 @@ import SignIn from "./SignIn";
 import Link from "next/link";
 import Image from "next/image";
 import SignOut from "./SignOut";
+import { determineTeam } from "./AuthSection";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "../theme/LoadingSpinner";
+import { TUser } from "@/lib/queries/user/user";
 
 export default function MobileAuth() {
+  const [user, setUser] = useState<TUser>();
+  const [loading, setLoading] = useState(false);
   const { data: session } = useSession();
+
+  async function getUserInfo() {
+    try {
+      setLoading(true);
+      const res = await fetch(`/api/player/user/${session?.user?.id}`);
+      if (res.ok) {
+        const data = await res.json();
+        setUser(data);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, [session]);
+
   if (!session)
     return (
       <>
@@ -18,24 +44,38 @@ export default function MobileAuth() {
       </>
     );
 
-  const userAvatar = session?.user?.image;
+  const userImage = session?.user?.image;
+  const userName = user?.name;
+  let team;
+  if (user) {
+    team = determineTeam(user);
+  } else {
+    team = "Loading...";
+  }
+
   return (
     <div className="flex flex-col gap-2 items-center">
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-2 ml-10">
         <div className="flex m-auto">
           <Link href="/me">
             <Image
               alt="user avatar"
-              src={userAvatar ?? ""}
+              src={userImage ?? ""}
               width={25}
               height={25}
               className="inline-block size-12 rounded-full"
             />
           </Link>
         </div>
-        <div className="flex flex-col text-gray-300 p-2 text-md">
-          <h1 className="italic">name</h1>
-          <h2 className="italic">team</h2>
+        <div className="flex flex-col text-gray-300 p-2 text-sm gap-1">
+          {loading ? (
+            <LoadingSpinner />
+          ) : (
+            <>
+              <h1 className="italic">{userName}</h1>
+              <h2 className="italic">{team}</h2>
+            </>
+          )}
         </div>
       </div>
 
