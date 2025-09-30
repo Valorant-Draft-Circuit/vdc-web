d5c6add97e7bcef06d3e6693ea2a5ea2963bf41c
Sooho <95513590+dlatngh@users.noreply.github.com>
2025-09-29 17:44:11 -0700

    pass tier as Tier (#135)

--- lib/queries/schedule/schedule.ts
+++ lib/queries/schedule/schedule.ts
@@ -63,7 +63,7 @@ export async function getScheduleByTier(tier: Tier, season: number) {
 async function getUpcomingMatchesDates(tier: Tier, season: number) {
   const upcomingMatches = await prisma.matches.findMany({
     where: {
-      tier: tier as Tier,
+      tier,
       season: season,
       matchType: MatchType.BO2,
       Home: {
