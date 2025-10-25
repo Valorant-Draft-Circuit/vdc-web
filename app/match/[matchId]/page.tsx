ad903f982bb463ba306f0bdc455757f63b56ef7b
Sooho <sooho.lim4@gmail.com>
2025-10-24 20:51:54 -0700

    map bans

--- app/match/[matchId]/page.tsx
+++ app/match/[matchId]/page.tsx
@@ -1,4 +1,9 @@
-import { TEAM_LOGOS_URL } from "@/lib/common/constants";
+import {
+  MAP_LIST_URL,
+  MAPS,
+  TEAM_LOGOS_URL,
+  TIER_COLOR_MAP,
+} from "@/lib/common/constants";
 import { toTailwindCustomHexCode } from "@/lib/common/utils";
 import { getMatch } from "@/lib/queries/match/match";
 import { Metadata } from "next";
@@ -27,12 +32,17 @@ export default async function Page({
   const homeTeam = matchInfo?.Home;
   const awayTeam = matchInfo?.Away;
 
+  const teams = {
+    home: homeTeam,
+    away: awayTeam,
+  };
+
   const homeTeamBrand = homeTeam?.Franchise.Brand;
   const awayTeamBrand = awayTeam?.Franchise.Brand;
 
+  const tierColor = TIER_COLOR_MAP[matchInfo!.tier];
   const homeTeamColor = toTailwindCustomHexCode(homeTeamBrand!.colorPrimary);
   const awayTeamColor = toTailwindCustomHexCode(awayTeamBrand!.colorPrimary);
-  console.log(matchInfo);
 
   const matchGames = matchInfo?.Games;
   let homeWins = 0,
@@ -41,8 +51,6 @@ export default async function Page({
     if (game.winner === homeTeam?.id) homeWins++;
     else awayWins++;
   });
-
-  console.log(homeWins, awayWins);
 
   const matchDate = new Date(matchInfo!.dateScheduled).toLocaleString(`en-US`, {
     month: `short`,
@@ -74,10 +82,13 @@ export default async function Page({
                 height={500}
                 className="size-25 xl:size-50 drop-shadow-lg"
               />
-              <div className="flex flex-row gap-5 italic my-auto text-4xl text-vdcRed">
-                <h1>{homeWins}</h1>
-                <h1>/</h1>
-                <h1>{awayWins}</h1>
+              <div className="flex flex-col text-2xl italic">
+                <h1 className={`text-${tierColor}`}>{matchInfo?.tier}</h1>
+                <div className="flex flex-row gap-5 italic m-auto text-4xl text-vdcRed">
+                  <h1>{homeWins}</h1>
+                  <h1>/</h1>
+                  <h1>{awayWins}</h1>
+                </div>
               </div>
 
               <Image
@@ -90,9 +101,60 @@ export default async function Page({
             </div>
           </div>
         </div>
-        <div className="flex flex-row justify-between text-vdcRed italic text-lg">
+        <div className="flex flex-row justify-between text-lg p-2">
           <h1>SEASON {matchInfo?.season} MATCH DAY 1</h1>
           <h1>{matchDate}</h1>
+        </div>
+        <div className="p-2">
+          <h1>MAP BANS / PICKS</h1>
+          <div className="flex flex-col gap-1">
+            {matchInfo?.MapBans.map((mapBan) => (
+              <MapBan key={mapBan.id} mapBan={mapBan} teams={teams} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+async function MapBan({ mapBan, teams }) {
+  const mapUrl = MAP_LIST_URL(MAPS[mapBan.map.toUpperCase()]);
+  const isBan = mapBan.type === "BAN";
+  const isDiscard = mapBan.type === "DISCARD";
+  const { home, away } = teams;
+  let team;
+  if (mapBan.team === home.id) {
+    team = home;
+  } else {
+    team = away;
+  }
+
+  return (
+    <div className="relative">
+      <Image
+        alt={mapBan.map}
+        src={mapUrl}
+        width={5000}
+        height={5000}
+        className={` absolute inset-0 -z-10 size-full object-cover brightness-30 ${
+          (isBan || isDiscard) && "grayscale"
+        }`}
+      />
+      <div className="flex flex-row italic p-5 gap-5 justify-between">
+        <div className="flex flex-row gap-5 drop-shadow-lg text-vdcWhite my-auto">
+          <h1>{mapBan.map}</h1>
+          <h1>-</h1>
+          <h1>{mapBan.type}</h1>
+        </div>
+        <div>
+          <Image
+            alt={team?.name}
+            src={`${TEAM_LOGOS_URL}${team?.Franchise.Brand.logo}`}
+            width={5000}
+            height={5000}
+            className="size-10"
+          />
         </div>
       </div>
     </div>
