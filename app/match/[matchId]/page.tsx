5562f76f31a056d7a024c58c9bdcdc0cf611d818
Sooho <95513590+dlatngh@users.noreply.github.com>
2025-10-25 15:37:55 -0700

    Dla/match page improvements (#179)
    * clean up stats table for games
    
    * remove logs

--- app/match/[matchId]/page.tsx
+++ app/match/[matchId]/page.tsx
@@ -170,7 +170,6 @@ export default async function Page({
 
 function MatchOverview({ gameOverview, teams }) {
   const mapUrl = SECONDARY_MAP_LIST_URL(MAPS[gameOverview.map.toUpperCase()]);
-  console.log(gameOverview);
   return (
     <>
       <h1>Match Overview</h1>
