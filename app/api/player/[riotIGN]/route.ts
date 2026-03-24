6e7cd6c1977581f54755534b81dcb86efea8c135
Sooho <95513590+dlatngh@users.noreply.github.com>
2026-03-23 19:56:29 -0700

    omit mmr from api payload (#210)
    * omit mmr from api payload
    
    * bump prissy

--- app/api/player/[riotIGN]/route.ts
+++ app/api/player/[riotIGN]/route.ts
@@ -1,14 +1,36 @@
-import { Player } from "@/prisma";
+import { ControlPanel, Player } from "@/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: Promise<{ riotIGN: string }> }
+  { params }: { params: Promise<{ riotIGN: string }> },
 ) {
   const riotIGN = (await params).riotIGN;
+  const shouldIncludeMmr = await ControlPanel.getMMRDisplayState();
   const player = await Player.getBy({ ign: riotIGN });
-  if (player) {
-    return NextResponse.json(player);
+
+  const transformAccount = (account) => {
+    if (!account) return account;
+
+    if (shouldIncludeMmr) {
+      return {
+        ...account,
+        MMR: account.MMR ? { mmrEffective: account.MMR.mmrEffective } : null,
+      };
+    }
+    // need to omit mmr from info
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { MMR, ...mmrExcludedAccountInfo } = account;
+    return mmrExcludedAccountInfo;
+  };
+
+  if (!player) {
+    return NextResponse.json({ error: "Not Found." }, { status: 404 });
   }
-  return NextResponse.json({ error: "Not Found." }, { status: 404 });
+
+  return NextResponse.json({
+    ...player,
+    PrimaryRiotAccount: transformAccount(player.PrimaryRiotAccount),
+    Accounts: player.Accounts?.map(transformAccount),
+  });
 }
